84816c85e59cdfef07aa9ee7563d30489dfff781
yuranet <yura.yuriev@gmail.com>
2014-02-28 16:32:19 +0400

    Public and private containers now supported.

--- Tools/UploadFilesToAzure/Program.cs
+++ Tools/UploadFilesToAzure/Program.cs
@@ -3,6 +3,7 @@ using Micajah.FileService.Tools.UploadFilesToAzure.MetaDataSetTableAdapters;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.Collections.Generic;
 using System.Configuration;
 using System.Globalization;
 using System.IO;
@@ -27,21 +28,26 @@ namespace Micajah.FileService.Tools.UploadFilesToAzure
                 client = new FileMtomServiceSoapClient();
                 adapter = new FileTableAdapter();
 
+                // Read configuration.
                 string cacheControl = string.Format(CultureInfo.InvariantCulture, "public, max-age={0}", ConfigurationManager.AppSettings["mafs:ClientCacheExpiryTime"]);
                 int uploadSpeedLimit = Convert.ToInt32(ConfigurationManager.AppSettings["UploadSpeedLimit"]);
                 int parallelOperationThreadCount = Convert.ToInt32(ConfigurationManager.AppSettings["ParallelOperationThreadCount"]);
                 string storageConnectionString = ConfigurationManager.AppSettings["mafs:StorageConnectionString"];
+                List<string> objectTypesWithPublicAccess = new List<string>(ConfigurationManager.AppSettings["ObjectTypesWithPublicAccess"].Split(','));
 
+                // Initialize and configure the storage and client.
                 CloudStorageAccount storageAccount = CloudStorageAccount.Parse(storageConnectionString);
                 CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
                 blobClient.ParallelOperationThreadCount = parallelOperationThreadCount;
                 blobClient.SingleBlobUploadThresholdInBytes = uploadSpeedLimit;
 
                 CloudBlobContainer container = null;
+                CloudBlobContainer publicContainer = null;
                 Guid departmentId = Guid.Empty;
                 int fileIndex = 0;
                 int rowsCount = 0;
 
+                // Get top 1000 files from the database.
                 table = adapter.GetFiles();
                 rowsCount = table.Count;
 
@@ -56,6 +62,7 @@ namespace Micajah.FileService.Tools.UploadFilesToAzure
 
                         try
                         {
+                            // Download the file from File Service.
                             Console.WriteLine("File #{0} \"{1}\" (fileUniqueId = {2}).", fileIndex, row.Name, row.FileUniqueId);
                             Console.Write("Getting from File Service...");
 
@@ -64,20 +71,56 @@ namespace Micajah.FileService.Tools.UploadFilesToAzure
                             Console.Write(" Done. ");
                             Console.Write("Uploading to Azure...");
 
+                            // Get the corresponding container for the file and create it if not exists.
                             if (row.DepartmentId != departmentId)
                             {
-                                container = blobClient.GetContainerReference(row.DepartmentId.ToString("N"));
-                                container.CreateIfNotExists();
+                                publicContainer = null;
+                                container = null;
 
                                 departmentId = row.DepartmentId;
+                            }
+
+                            bool publicAccess = objectTypesWithPublicAccess.Contains(row.LocalObjectType);
+                            CloudBlobContainer blobContainer = null;
+
+                            if (publicAccess)
+                            {
+                                if (publicContainer == null)
+                                {
+                                    string containerName = string.Format(CultureInfo.InvariantCulture, "{0:N}p", row.DepartmentId);
+
+                                    publicContainer = blobClient.GetContainerReference(containerName);
+                                    publicContainer.CreateIfNotExists();
+
+                                    BlobContainerPermissions p = new BlobContainerPermissions()
+                                    {
+                                        PublicAccess = BlobContainerPublicAccessType.Blob
+                                    };
+                                    publicContainer.SetPermissions(p);
+                                }
+
+                                blobContainer = publicContainer;
+                            }
+                            else
+                            {
+                                if (container == null)
+                                {
+                                    string containerName = row.DepartmentId.ToString("N");
+
+                                    container = blobClient.GetContainerReference(containerName);
+                                    container.CreateIfNotExists();
+                                }
+
+                                blobContainer = container;
                             }
 
                             string blobName = string.Format(CultureInfo.InvariantCulture, "{0}/{1}/{2}", row.LocalObjectType, row.LocalObjectId, row.Name);
                             string mimeType = System.Web.MimeMapping.GetMimeMapping(row.Name);
 
+                            // Upload to Azure.
                             using (Stream stream = new MemoryStream(fileResponse.fileData))
                             {
-                                CloudBlockBlob blob = container.GetBlockBlobReference(blobName);
+                                CloudBlockBlob blob = blobContainer.GetBlockBlobReference(blobName);
                                 blob.Properties.ContentType = mimeType;
                                 blob.Properties.CacheControl = cacheControl;
                                 blob.UploadFromStream(stream);
@@ -93,9 +136,11 @@ namespace Micajah.FileService.Tools.UploadFilesToAzure
                             Console.WriteLine(" Failed.\r\n{0}\r\n", ex.ToString());
                         }
 
+                        // Update upload status of the file in the database.
                         adapter.UpdateUploadStatus(row.FileUniqueId, uploadStatus);
                     }
 
+                    // Get next 1000 files from the database.
                     table = adapter.GetFiles();
                     rowsCount = table.Count;
                 }
